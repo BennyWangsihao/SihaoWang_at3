ef0caa95a5f57a04ebfd9e89e9d6a17cc1015605
BennyWangsihao <162288670+BennyWangsihao@users.noreply.github.com>
2026-05-26 15:53:51 +1000

    1

---
+++ script.js
@@ -0,0 +1,8 @@
+const audio = document.querySelector("#custom-audio-player");
+const playBtn = document.querySelector("#play-btn");
+const playImg = document.querySelector("#play-img");
+const muteBtn = document.querySelector("#mute-btn");
+const progress = document.querySelector("#progress");
+const progressFill = document.querySelector("#progress-fill");
+
+playImg.src=""
